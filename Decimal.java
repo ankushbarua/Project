e2531399209cb9c55f322475bfea78e2f5a9472f
ankushbarua <42117328+ankushbarua@users.noreply.github.com>
2018-08-05 21:34:22 +0530

    Add files via upload
    Addition of decimal numbers in float format.

---
+++ Decimal.java
@@ -0,0 +1,12 @@
+package gitup;
+
+public class Decimal 
+{
+       public static void main(String a[])
+       {
+       float num1=5.3f;
+       float num2=6.5f;
+       num2=7.7f;
+       System.out.println(num1+num2);
+       }
+}
