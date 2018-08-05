b66061f98a8e8ae4a5ad58a21d00e4fad95ff0cd
ankushbarua <42117328+ankushbarua@users.noreply.github.com>
2018-08-05 21:24:32 +0530

    Add files via upload
    Addition of two numbers.

---
+++ Add.java
@@ -0,0 +1,12 @@
+package gitup;
+
+public class Add 
+{
+      public static void main(String a[])
+      {
+    	     int num1=3;
+    	     int num2=5;
+    	     num2=7;
+             System.out.println(num1+num2);
+      }
+}
