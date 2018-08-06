bcb91f8c74acef9d9b1338e9ccb042b4665a7f8e
ankushbarua <42117328+ankushbarua@users.noreply.github.com>
2018-08-06 12:58:31 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package com.ankush.javascratchpad;
+/* binary lierals
+ * before java 7
+ */
+
+public class Demo {
+	public static void main(String a [])
+	{
+	int i = Integer.parseInt("101",2);
+	
+	System.out.println(i);
+	}
+
+}
