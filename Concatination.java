b34ae33e9c18faa0a8b423fdada6bc3e5609ef29
ankushbarua <42117328+ankushbarua@users.noreply.github.com>
2018-08-06 12:03:13 +0530

    Add files via upload

---
+++ Concatination.java
@@ -0,0 +1,13 @@
+package com.ankush.javascratchpad;
+
+public class Concatination 
+{
+	   public static void main(String a[])
+	   {
+       int i,j,k;
+       i=5;
+       j=4;
+       k=i+j;
+       System.out.println("The addition of two numbers "+i+" and "+j+" is"+k);
+	   }
+}
