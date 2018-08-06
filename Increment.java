89ff7da258fd77f85d9bc28f605365a4d7a87998
ankushbarua <42117328+ankushbarua@users.noreply.github.com>
2018-08-06 21:11:55 +0530

    Add files via upload

---
+++ Increment.java
@@ -0,0 +1,16 @@
+package com.ankush.javascratchpad;
+
+public class Increment 
+{
+       public static void main(String a[])
+       {
+       int i = 5;
+       int temp;
+       temp=i;
+       i++;
+       i=temp;
+       
+       
+       System.out.println(i);
+       }
+}
