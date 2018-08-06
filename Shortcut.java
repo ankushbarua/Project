f734df129e8f222b4af9d327370dfe42c6272f4b
ankushbarua <42117328+ankushbarua@users.noreply.github.com>
2018-08-06 12:20:28 +0530

    Add files via upload

---
+++ Shortcut.java
@@ -0,0 +1,15 @@
+package com.ankush.javascratchpad;
+//Short hand operators
+
+public class Shortcut 
+{
+	   public static void main(String a[])
+	   {
+	    byte b=8;
+	//    b = b+2; // not possible as it cant convert int to byte
+	    b += 2;
+	    
+	    System.out.println(b);
+		
+	   }
+}
